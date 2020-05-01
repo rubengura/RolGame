2997ebb65e2814c4bfdb44d4c87338529b094a5f
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-05-01 13:51:59 +0200

    Refactorizado código para crear un personaje con el nombre insertado por el jugador.

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ __author__ = "rgr"
 
 from acciones import tomar_eleccion
 from personaje import Personaje
-personaje=Personaje("Sergio")
-print(personaje.nombre)
 
 print("Bienvenido a Role Game!")
 pj_name = input("Introduce el nombre de tu personaje: ")
-print(f"Hola {pj_name}")
+pj = Personaje(nombre=pj_name)
+print(f"Hola {pj.nombre}")
 
 tomar_eleccion()
