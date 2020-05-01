a2734d2804940fb41add5321cdb6ab6d4d8fee0e
OigresMC <sergio.moreno.castillo@gmail.com>
2020-05-01 13:39:17 +0200

    Añadida clase de personaje

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 __author__ = "rgr"
 
 from acciones import tomar_eleccion
+from personaje import Personaje
+personaje=Personaje("Sergio")
+print(personaje.nombre)
 
 print("Bienvenido a Role Game!")
 pj_name = input("Introduce el nombre de tu personaje: ")
---
+++ personaje.py
@@ -0,0 +1,4 @@
+class Personaje:
+    def __init__(self,nombre):
+        self.nombre=nombre
+        self.hp=100
