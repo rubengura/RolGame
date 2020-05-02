068cc5469af60e529b714978ddb0accbc1d19117
rubengura <ruben.guerrero.ramirez@gmail.com>
2020-05-02 18:05:44 +0200

    Añadidos atributos de personaje y añadido método subir_nivel.

--- personaje.py
+++ personaje.py
@@ -2,3 +2,16 @@ class Personaje:
     def __init__(self, nombre):
         self.nombre = nombre
         self.hp = 100
+        self.pm = 50
+        self.fuerza = 20
+        self.habilidad = 15
+        self.carisma = 15
+        self.nivel = 1
+
+    def subir_nivel(self):
+        self.nivel += 1
+        self.hp = int(self.hp * 1.10)
+        self.pm = int(self.pm * 1.05)
+        self.fuerza += 2
+        self.habilidad += 1
+        self.carisma += 1
